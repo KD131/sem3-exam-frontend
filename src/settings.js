92ecd235d080a13d4dba52b33a72c7b132c8bb42
KD131 <71063280+KD131@users.noreply.github.com>
2022-01-13 01:52:53 +0100

    adds simple deploy script

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,4 @@
-const SERVER_URL = "http://localhost:8080/startcode";
+// const SERVER_URL = "http://localhost:8080/startcode";
+const SERVER_URL = "https://kavv.dk/tomcat/startcode"
 
 export { SERVER_URL };
